054b2a86d18c0bc8c3e34b99d8da0ab682ad4ab1
camvlira <127799093+camvlira@users.noreply.github.com>
2023-06-07 12:54:02 +0000

    questao 1

---
+++ Atividade5/Questao1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    FILE *arq;
+    char caract;
+
+   
+    arq = fopen("arq.txt", "w");
+
+   
+    printf("Digite os caracteres a serem gravados no arquivo e digite '0' para finalizar: \n");
+    while (1) {
+        scanf(" %c", &caract);
+        if (caract == '0') {
+            break;
+        }
+        fputc(caract, arq);
+    }
+    
+    fclose(arq);
+    
+    arq = fopen("arq.txt", "r");
+    printf("Caracteres armazenados:\n");
+    while ((caract = fgetc(arq)) != EOF) {
+        printf("%c", caract);
+    }
+
+    fclose(arq);
+
+    return 0;
+}
