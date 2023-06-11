a0708d3d88daa661dad35d6d7ea6d808aefd0895
camvlira <127799093+camvlira@users.noreply.github.com>
2023-06-11 03:23:39 +0000

    processo

---
+++ Atividade1/Questao1.c
@@ -0,0 +1,14 @@
+//1) Imprime a diagonal principal de uma matriz quadrada qualquer.
+#include <stdio.h>
+
+int main() {
+  int matriz[3][3] = {{5, 2, 3}, {1, 5, 2}, {7, 1, 1}};
+  int i;
+
+  printf("Diagonal:\n");
+  for (i = 0; i < 3; i++) {
+    printf("%i\n", matriz[i][i]);
+ } 
+
+  return 0;
+}
---
+++ Atividade2/Questao1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void matrizSum(int n, int MatA[][n], int MatB[][n], int MatC[][n]) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            MatC[i][j] = MatA[i][j] + MatB[i][j];
+        }}}
+int main() {
+    int tamMatriz = 3;
+    int Mat1[3][3] = {{1, 5, 3}, {2, 5, 1}, {7, 1, 3}};
+    int Mat2[3][3] = {{1, 3, 3}, {1, 6, 3}, {3, 4, 1}};
+    int MatSum[3][3];
+
+    matrizSum(tamMatriz, Mat1, Mat2, MatSum);
+    
+    printf("Matriz A:\n");
+    for (int i = 0; i < tamMatriz; i++) {
+        for (int j = 0; j < tamMatriz; j++) {
+          printf("%i ", Mat1[i][j]);
+        } printf("\n");}
+
+        printf("Matriz B:\n");
+    for (int i = 0; i < tamMatriz; i++) {
+        for (int j = 0; j < tamMatriz; j++) {
+          printf("%i ", Mat2[i][j]);
+        } printf("\n");}
+
+        printf("Soma da matrizes:\n");
+    for (int i = 0; i < tamMatriz; i++) {
+        for (int j = 0; j < tamMatriz; j++) {
+          printf("%i ", MatSum[i][j]);
+        } printf("\n");}
+    
+    return 0;
+}
